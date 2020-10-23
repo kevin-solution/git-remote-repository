b6716e4924143b0c83ba9dabaa4cea408517196a
kevin-solution <kevin.solution41@gmail.com>
2020-10-23 12:10:56 -0400

    Update Resume

---
+++ bot.sh
@@ -0,0 +1,31 @@
+for Y in {2019..2019}
+do
+  mkdir $Y
+  cd $Y
+  for M in {03..03}
+  do
+    mkdir $M
+    cd $M
+    for D in {30..30}
+    do
+      mkdir $D
+      cd $D
+      for i in {01..10}
+      do
+        echo "$i on $M/$D/$Y" > commit.md
+        export GIT_COMMITTER_DATE="$Y-$M-$D 12:$i:00"
+        export GIT_AUTHOR_DATE="$Y-$M-$D 12:$i:00"
+        git add commit.md -f
+        git commit --date="$Y-$M-$D 12:0$i:00" -m "$i on happy birthday"        
+      done
+      cd ../
+    done
+    cd ../
+  done
+  cd ../
+done
+git push origin main
+git rm -rf 20**
+git rm -rf 19**
+git commit -am "cleanup"
+git push origin main
